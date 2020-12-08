0237abd077d04931e76dc80db8df1750576ce9ab
Semih <semihtr13@gmail.com>
2020-12-08 16:06:11 +0300

    Builder design pattern

--- src/Customer.java
+++ src/Customer.java
@@ -3,10 +3,10 @@ public class Customer {
     private String email;
     private String age;
 
-    public Customer(Builder builder){
-        this.name=builder.name;
-        this.email=builder.email;
-        this.age=builder.age;
+    public Customer(Builder builder) {
+        this.name = builder.name;
+        this.email = builder.email;
+        this.age = builder.age;
     }
 
     public String getName() {
@@ -21,21 +21,40 @@ public class Customer {
         return age;
     }
 
-    public static class Builder{
+    public static class Builder {
         private String name;
         private String email;
         private String age;
 
-        /**
-         *
-         * @param name Required field
-         */
-        public Builder(String name){
-            this.name=name;
+
+        public Builder() {
+
         }
 
         /**
-         *
+         * @param name Required field
+         */
+        public Builder(String name) {
+            this.name = name;
+        }
+
+        /**
+         * @param name Required field
+         */
+        public Builder(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        /**
+         * @param name Optional Field
+         */
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        /**
          * @param email Optional field
          */
         public Builder setEmail(String email) {
@@ -44,7 +63,6 @@ public class Customer {
         }
 
         /**
-         *
          * @param age Optional Field
          */
         public Builder setAge(String age) {
@@ -52,11 +70,14 @@ public class Customer {
             return this;
         }
 
-        public Customer buid(){
+        public Customer buid() {
             return new Customer(this);
         }
 
-
     }
 
+    @Override
+    public String toString() {
+        return String.format("Customer { Name : %s , Email: %s , Age: %s }",this.name,this.email,this.age);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Customer customer=new Customer
+        Customer customer1=new Customer
                 .Builder("Semih")
                 .setEmail("semihtr13@gmail.com")
                 .setAge("25")
                 .buid();
-        System.out.printf("Name : %s , Email: %s , Age: %s",customer.getName(),customer.getEmail(),customer.getAge());
+
+        System.out.println(customer1);
+
+        Customer customer2=new Customer.Builder()
+                .setName("Orhan")
+                .setAge("21")
+                .buid();
+
+
+        System.out.println(customer2);
     }
 }
